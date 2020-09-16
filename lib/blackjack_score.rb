fbfa4324826c3aa2d47df755c979220600e5e6d6
Iris Lux <iris.lux0@gmail.com>
2020-09-16 02:40:42 -0700

    wrote blackjack_score  method and some tests

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -3,5 +3,34 @@
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
 def blackjack_score(hand)
+  #how do we translate roaylty into 10s?
+  # how do we determine the value of Aces?
+  # write three argument errors -
+  values_hand = royalty_to_value(hand)
+  value = values_hand.sum
+
+  if (values_hand.include?(11) && value > 21)
+    i = 0
+    while( value > 21 && i < values_hand.count(11))
+      value -= 10
+      i += 1
+    end
+  end
+    #Exception here
+
+  return value
 
 end
+
+def royalty_to_value (hand)
+  return hand.map do |card|
+    if(card == 'Ace')
+      11
+    elsif(%w[Jack Queen King].include?(card))
+      10
+    else
+      card
+    end
+  end
+end
+
--- test/blackjack_score_test.rb
+++ test/blackjack_score_test.rb
@@ -18,26 +18,59 @@ describe 'Blackjack Score' do
     score = blackjack_score(hand)
 
     # Assert <-  You do this part!
-
+    expect(score).must_equal(7)
   end
 
   it 'facecards have values calculated correctly' do
+    # Arrange
+    hand = ['Jack', 'Queen']
 
+    # Act
+    score = blackjack_score(hand)
+
+    # Assert <-  You do this part!
+    expect(score).must_equal(20)
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
+    # Arrange
+    hand = [5, 5, 'Ace']
 
+    # Act
+    score = blackjack_score(hand)
+
+    # Assert <-  You do this part!
+    expect(score).must_equal(21)
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
+    # Arrange
+    hand = [5, 'Ace', 'Ace', 'Ace', 'Ace']
 
+    # Act
+    score = blackjack_score(hand)
+
+    # Assert <-  You do this part!
+    expect(score).must_equal(19)
   end
 
   it 'raises an ArgumentError for invalid cards' do
+    expect {
+      blackjack_score([5, 6, 30])
+    }.must_raise ArgumentError
 
+    expect {
+      blackjack_score(['princess', 4])
+    }.must_raise ArgumentError
+
+    expect {
+      blackjack_score([nil, nil, nil])
+    }.must_raise ArgumentError
   end
 
   it 'raises an ArgumentError for scores over 21' do
-
+    expect {
+      blackjack_score([4, 10, 'Jack', 'Jack'])
+    }.must_raise ArgumentError
   end
 end
